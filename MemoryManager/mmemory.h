8416f87b090933aa1a0a6ef117838f06a2791448
Nastassia <nastassialmezhen@gmail.com>
2017-09-06 13:36:49 +0300

    Add project files.

---
+++ MemoryManager/mmemory.h
@@ -0,0 +1,13 @@
+#include <stddef.h>
+
+typedef char* VA;				
+
+int _malloc(VA* ptr, size_t szBlock);
+
+int _free(VA ptr);
+
+int _read(VA ptr, void* pBuffer, size_t szBuffer);
+
+int _write(VA ptr, void* pBuffer, size_t szBuffer);
+
+int _init(int n, int szPage);
